36603b75106dd7ab0cb0d465e95b79d2adf67a76
Kira <kira.eva@protonmail.com>
2022-09-09 23:01:26 +0100

    change color with arrowkeys and space

--- examples/game.rs
+++ examples/game.rs
@@ -1,3 +1,11 @@
+use rust_engine::Key;
+
 fn main() {
-    rust_engine::clear_screen_to_color(0.0, 0.0, 1.0, 1.0);
+    rust_engine::set_event_handler(move |key| match key {
+        Key::Left => rust_engine::clear_screen_to_color(1.0, 0.0, 0.0, 1.0),
+        Key::Right => rust_engine::clear_screen_to_color(0.0, 1.0, 0.0, 1.0),
+        Key::Up => rust_engine::clear_screen_to_color(0.0, 0.0, 1.0, 1.0),
+        Key::Down => rust_engine::clear_screen_to_color(0.0, 1.0, 1.0, 1.0),
+        Key::Space => rust_engine::clear_screen_to_color(1.0, 1.0, 0.0, 1.0),
+    })
 }
